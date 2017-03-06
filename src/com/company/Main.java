3f6087392b5f480b974cbaa7c879fd86cc4b115c
My first project on github <o-xana@yandex.ru>
2017-03-06 20:23:31 +0300

    Added new line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        System.out.println("Hello Github");
+        System.out.println("Add one more line");
     }
 }
